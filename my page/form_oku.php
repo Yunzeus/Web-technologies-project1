921eae0a7b8e2ade7cb91bdebe0626f65a2c81a3
yunus3444 <54690790+yunus3444@users.noreply.github.com>
2020-04-28 04:37:52 +0300

    new 4
     son olarak css

---
+++ my page/form_oku.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<header>
+</header>
+<body>
+<?php
+         $_isim=$_POST['isim'];
+             $_soyisim=$_POST['soyisim'];
+         echo "Hoşgeldin $_isim $_soyisim";
+?>
+</body>
+</html>
